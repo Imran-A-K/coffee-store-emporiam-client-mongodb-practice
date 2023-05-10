6b2c65b8042d08824f609392c9bf8844db4b0d95
Imran-A-K <imran.k.professional@gmail.com>
2023-05-10 20:14:36 +0600

    first commit

---
+++ src/components/AddCoffee.jsx
@@ -0,0 +1,168 @@
+import Swal from 'sweetalert2'
+const AddCoffee = () => {
+  const handleAddCoffee = event => {
+    event.preventDefault();
+    const form = event.target;
+    const name = form.name.value;
+    const quantity = form.quantity.value;
+    const supplier = form.supplier.value;
+    const taste = form.taste.value;
+    const category = form.category.value;
+    const details = form.details.value;
+    const photo = form.photo.value;
+    const newCoffee = {
+      name,
+      quantity,
+      supplier,
+      taste,
+      category,
+      details,
+      photo
+    }
+    console.log(newCoffee);
+    //send data to backend
+    fetch(`http://localhost:5000/coffee`, {
+      method: 'POST',
+      headers: {
+        'content-type': 'application/json'
+      },
+      body: JSON.stringify(newCoffee)
+    })
+    .then(response => {
+      return response.json()
+    })
+    .then(data =>{
+      console.log(data)
+      if(data.insertedId){
+        Swal.fire({
+          title: 'Success!',
+          text: 'Coffee added successfully',
+          icon: 'success',
+          confirmButtonText: 'Cool'
+        })
+      }
+    })
+  }
+  return (
+    <div className="bg-[#F3F3F0] p-24">
+      <h2 className="text-3xl font-extrabold">Add a Coffee</h2>
+      <form onSubmit={handleAddCoffee}>
+        {/* form name and quantity row */}
+        <div className="flex mb-8">
+          <div className="form-control md:w-1/2">
+            <label className="label">
+              <span className="label-text">Coffee Name</span>
+            </label>
+            <label className="input-group">
+              <span>Name</span>
+              <input
+                type="text"
+                name="name"
+                placeholder="Coffee Name"
+                className="input input-bordered w-full"
+              />
+            </label>
+          </div>
+          <div className="form-control ml-4 md:w-1/2">
+            <label className="label">
+              <span className="label-text">Available Quantity</span>
+            </label>
+            <label className="input-group">
+              <span>Quantity</span>
+              <input
+                type="text"
+                name="quantity"
+                placeholder="Coffee Quantity"
+                className="input input-bordered w-full"
+              />
+            </label>
+          </div>
+        </div>
+        {/* form supplier row */}
+        <div className="flex mb-8">
+          <div className="form-control md:w-1/2">
+            <label className="label">
+              <span className="label-text">Supplier</span>
+            </label>
+            <label className="input-group">
+              <span>Supplier</span>
+              <input
+                type="text"
+                name="supplier"
+                placeholder="Supplier Name"
+                className="input input-bordered w-full"
+              />
+            </label>
+          </div>
+          <div className="form-control ml-4 md:w-1/2">
+            <label className="label">
+              <span className="label-text">Taste</span>
+            </label>
+            <label className="input-group">
+              <span>Taste</span>
+              <input
+                type="text"
+                name="taste"
+                placeholder="Taste"
+                className="input input-bordered w-full"
+              />
+            </label>
+          </div>
+        </div>
+        {/* form category and details */}
+        <div className="flex mb-8">
+          <div className="form-control md:w-1/2">
+            <label className="label">
+              <span className="label-text">Category</span>
+            </label>
+            <label className="input-group">
+              <span>Category</span>
+              <input
+                type="text"
+                name="category"
+                placeholder="Category"
+                className="input input-bordered w-full"
+              />
+            </label>
+          </div>
+          <div className="form-control ml-4 md:w-1/2">
+            <label className="label">
+              <span className="label-text">Category Details</span>
+            </label>
+            <label className="input-group">
+              <span>Details</span>
+              <input
+                type="text"
+                name="details"
+                placeholder="Category Details"
+                className="input input-bordered w-full"
+              />
+            </label>
+          </div>
+        </div>
+        {/* form photo Url row */}
+        <div className="mb-8">
+          <div className="form-control w-full">
+            <label className="label">
+              <span className="label-text">Photo Url</span>
+            </label>
+            <label className="input-group">
+              <span>URL</span>
+              <input
+                type="text"
+                name="photo"
+                placeholder="Photo URL"
+                className="input input-bordered w-full"
+              />
+            </label>
+          </div>
+          
+        </div>
+        
+        <input type="submit" className="btn btn-block" value="Add Coffee" />
+      </form>
+    </div>
+  );
+};
+
+export default AddCoffee;
---
+++ src/components/CoffeeCard.jsx
@@ -0,0 +1,72 @@
+import { Link } from "react-router-dom";
+import Swal from "sweetalert2";
+
+const CoffeeCard = ({ coffee, coffees, setCoffees }) => {
+
+  const {_id, name, quantity, supplier, taste, category, details, photo } = coffee;
+  const handleDelete = _id => {
+    console.log(_id)
+    Swal.fire({
+        title: 'Are you sure?',
+        text: "You won't be able to revert this!",
+        icon: 'warning',
+        showCancelButton: true,
+        confirmButtonColor: '#3085d6',
+        cancelButtonColor: '#d33',
+        confirmButtonText: 'Yes, delete it!'
+      }).then((result) => {
+        if (result.isConfirmed) {
+        //   Swal.fire(
+        //     'Deleted!',
+        //     'Your file has been deleted.',
+        //     'success'
+        //   )
+        // console.log('delete confirmed ')
+        fetch(`http://localhost:5000/coffee/${_id}`,{
+          method: 'DELETE'
+        })
+        .then(response => response.json())
+        .then(data => {
+          console.log(data)
+          if(data.deletedCount > 0){
+            Swal.fire(
+                  'Deleted!',
+                  'Your Coffee has been deleted.',
+                  'success'
+                )
+                const remaining = coffees.filter( cof => cof._id !== _id)
+                setCoffees(remaining)
+          }
+        })
+        }
+      })
+  }
+  return (
+    <div className="card card-side bg-base-100 shadow-xl">
+      <figure>
+        <img src={photo} alt="Movie" />
+      </figure>
+      <div className="flex justify-between w-full pr-4">
+        <div>
+        <h2 className="card-title">Name: {name}</h2>
+        <p>{quantity}</p>
+        <p>{supplier}</p>
+        <p>{taste}</p>
+        </div>
+        <div className="card-actions justify-end">
+          <div className="btn-group btn-group-vertical space-y-4">
+            <button className="btn">View</button>
+            <Link to={`updateCoffee/${_id}`}>
+            <button className="btn">Edit</button>
+            </Link>
+            <button 
+            onClick={() => handleDelete(_id)}
+            className="btn bg-orange-500">X</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default CoffeeCard;
